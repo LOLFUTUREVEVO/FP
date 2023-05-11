1cc3a06a068dcabe9ddd0fd8b98c91ed64dda350
KarolKopciuch <kkop3228@flhs.us>
2023-05-11 09:24:06 -0400

    - Added assets to project

--- HelloAndAgain/src/Game/Player/AwakePlayer.java
+++ HelloAndAgain/src/Game/Player/AwakePlayer.java
@@ -1,11 +1,23 @@
 package Game.Player;
 
 
+import processing.core.PApplet;
+import processing.core.PImage;
 
-public class AwakePlayer extends Player{
+import java.awt.*;
+
+public class AwakePlayer extends Player {
+    PImage image;
+
+
     // This wont have anything additional besides certain objective
     public AwakePlayer(int startX, int startY, int HealthMax) {
         super(startX, startY, HealthMax);
         this.setEnableLeveling(true);
+        image = new PImage();
+        image = PImage.loadTGA()
     }
+
+
+
 }
