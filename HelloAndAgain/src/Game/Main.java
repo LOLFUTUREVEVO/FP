36b4c1ea9ee736ee98fac837cfca5185ed1be649
KarolKopciuch <kkop3228@flhs.us>
2023-05-12 15:45:41 -0400

    -- Added character sprite sheet found in assets as Character.png

--- HelloAndAgain/src/Game/Main.java
+++ HelloAndAgain/src/Game/Main.java
@@ -3,25 +3,40 @@ package Game;
 import Game.Player.AwakePlayer;
 import Game.Player.DreamPlayer;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Main extends PApplet {
     public AwakePlayer plrA;
     public DreamPlayer plrD;
-
+    int frameC = 0;
+    PImage crosshair;
     public void settings() {
-        size(1000,1000);
+        size(1920,1080);
         fullScreen();
     }
 
     public void setup() {
-        plrA = new AwakePlayer(width/2, height/2 , 100, this, loadImage("Game/Assets/Character/Idle/CharFrameOneIdle.png", "png"));
+        plrA = new AwakePlayer(width/2 - 32, height/2 - 32 , 100, this, loadImage("Game/Assets/Character/Idle/CharFrameOneIdle.png", "png") , loadImage("Game/Assets/Character/Idle/CharFrameTwoIdle.png", "png"));
+        crosshair = loadImage("Game/Assets/Crosshair.png");
+
     }
 
 
     public void draw() {
+        noCursor();
+
         background(0);
+
         plrA.displayPlayer(this);
+
+        if(frameC > 20) {
+            plrA.AnimatorAwake();
+            frameC = 0;
+        }
+
         plrA.getCharPos().checkMax();
+        image(crosshair, mouseX - 42,mouseY - 42);
+        frameC++;
     }
 
     public void keyPressed() {
--- HelloAndAgain/src/Game/Player/AwakePlayer.java
+++ HelloAndAgain/src/Game/Player/AwakePlayer.java
@@ -9,21 +9,36 @@ import java.awt.*;
 import static processing.awt.ShimAWT.loadImage;
 
 public class AwakePlayer extends Player {
-    PImage image;
+    public PImage frame1, frame2;
+
+    private int lol = 0;
+    PImage[] imgarr = new PImage[2];
 
 
     // This wont have anything additional besides certain objective
-    public AwakePlayer(int startX, int startY, int HealthMax, PApplet main, PImage img) {
+    public AwakePlayer(int startX, int startY, int HealthMax, PApplet main, PImage img, PImage f2) {
         super(startX, startY, HealthMax);
         this.setEnableLeveling(true);
-        image = new PImage();
-        image = img;
-        img.resize(128,128);
+        frame1 = new PImage();
+        frame2 = new PImage();
+        frame1 = img;
+        frame2 = f2;
+
+
+        imgarr[0] = frame1;
+        imgarr[1] = f2;
     }
     public void displayPlayer(PApplet main) {
-        main.image(image, super.getCharPos().getX(), super.getCharPos().getY());
-    }
 
+        main.image(imgarr[lol], super.getCharPos().getX(), super.getCharPos().getY());
+    }
+    public void AnimatorAwake() {
+        if(lol == 0) {
+            lol++;
+        } else {
+            lol--;
+        }
+    }
 
 
 }
