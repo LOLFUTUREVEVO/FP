4a20b254ac9de280b3c35677eb0f1ffcb1297dc2
KarolKopciuch <kkop3228@flhs.us>
2023-05-19 13:43:36 -0400

    -- Character anim frames redone

--- HelloAndAgain/src/Game/Player/AwakePlayer.java
+++ HelloAndAgain/src/Game/Player/AwakePlayer.java
@@ -4,33 +4,29 @@ package Game.Player;
 import processing.core.PApplet;
 import processing.core.PImage;
 
-import java.awt.*;
-
-import static processing.awt.ShimAWT.loadImage;
-
 public class AwakePlayer extends Player {
     public PImage frame1, frame2;
 
     private int lol = 0;
-    PImage[] imgarr = new PImage[2];
+    PImage[] IdleAnim = new PImage[2];
+    PImage[] WalkingAnim = new PImage[5];
 
 
     // This wont have anything additional besides certain objective
     public AwakePlayer(int startX, int startY, int HealthMax, PApplet main, PImage img, PImage f2) {
         super(startX, startY, HealthMax);
         this.setEnableLeveling(true);
-        frame1 = new PImage();
-        frame2 = new PImage();
+
         frame1 = img;
         frame2 = f2;
 
 
-        imgarr[0] = frame1;
-        imgarr[1] = f2;
+        IdleAnim[0] = frame1;
+        IdleAnim[1] = f2;
     }
     public void displayPlayer(PApplet main) {
 
-        main.image(imgarr[lol], super.getCharPos().getX(), super.getCharPos().getY());
+        main.image(IdleAnim[lol], super.getCharPos().getX(), super.getCharPos().getY());
     }
     public void AnimatorAwake() {
         if(lol == 0) {
