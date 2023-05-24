840db1f9c7e2bfc39efcc78ac112005e579ec78b
KarolKopciuch <kkop3228@flhs.us>
2023-05-24 09:22:52 -0400

    Bruh moment i was actually trolling

--- HelloAndAgain/src/Game/Player/Animator.java
+++ HelloAndAgain/src/Game/Player/Animator.java
@@ -1,12 +1,15 @@
 package Game.Player;
 
+import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Animator {
 
     PImage[] animationTrack;
 
-    public Animator() {
+
+    public Animator(int animLength, PApplet main) {
+        animationTrack = new PImage[animLength];
 
     }
 
